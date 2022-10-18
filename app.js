c4855088702fdf494bf30a16ae507ed30b6d82e5
Ashish Kumar Gupta <82220764+Proashish@users.noreply.github.com>
2022-10-18 15:39:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+const questions =[
+    {
+        'que':'which one of the following is markup language?',
+        'a':'HTML',
+        'b':'CSS',
+        'c':'Javascirpt',
+        'd':'PHP',
+        'correct':'a'
+        
+    },
+    {
+        'que':'What year was Javascript launched ',
+        'a':"1996",
+        'b': "1995",
+        'c':"2020",
+        'd':"2022",
+        'correct':'b'
+    },
+    {
+        'que':'What does CSS stands for',
+        'a':"HyperText Markup Language",
+        'b':"Cascading Style Sheet",
+        'c':"Jason Object Notation",
+        'd':"Helicopters Terminals Motorboats Lamborginis",
+        'correct':"b"
+
+    }
+
+]
+
+let index =0;
+let total = questions.length;
+let right =0,wrong =0;
+const quesBox =document.getElementById("quesBox")
+const optionInputs = document.querySelectorAll('.options')
+const loadQuestion  =()=>{
+    if(index == total){
+        return endQuiz();
+    }
+    else{
+    reset();
+}
+    const data  =questions[index]
+   quesBox.innerText =`${index+1}) ${data.que}`;
+   optionInputs[0].nextElementSibling.innerText =data.a;//we are getting label tag using this nextElemmentSibling funtion 
+   optionInputs[1].nextElementSibling.innerText =data.b;
+   optionInputs[2].nextElementSibling.innerText =data.c;
+   optionInputs[3].nextElementSibling.innerText =data.d;
+}
+
+
+const submitQuiz =()=>{
+    const data =questions[index];
+    const ans = getAnswer() 
+     if(ans === data.correct){
+        right++;
+     }
+     else{
+        wrong++;
+     }
+     index++;
+     loadQuestion();
+     return;
+}
+
+const getAnswer =()=>{
+     let answer ;
+    optionInputs.forEach(
+        (input) =>{
+            if(input.checked) {
+                answer = input.value;
+                
+            }
+        }
+    )
+    return answer;
+}
+
+const reset = () => {
+     optionInputs.forEach(
+        (input) => {
+            input.checked = false
+        }
+     )
+   }
+
+
+ const endQuiz = ()=>{
+    document.getElementById("box").innerHTML = `
+    <div style ="text-align:center">
+       <h3> Thank you for playing the quiz </h3>
+       <h2> ${right}/${total} are correct  </h2> 
+       </div>`
+ }
+//intial call
+loadQuestion();
